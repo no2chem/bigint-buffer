0c56805cd89663f2cdc5fb85f7f9e0fa5a0b3b7b
Michael Wei <mwei@cs.ucsd.edu>
2018-09-18 02:51:07 -0700

    :art: types - use primitive type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 interface ConverterInterface {
-  toBigInt(buf: Buffer, bigEndian?: boolean): BigInt;
+  toBigInt(buf: Buffer, bigEndian?: boolean): bigint;
   fromBigInt(num: BigInt, buf: Buffer, bigEndian?: boolean): Buffer;
 }
 
@@ -22,7 +22,7 @@ if (!process.browser) {
  * @param buf The little-endian buffer to convert
  * @returns A BigInt with the little-endian representation of buf.
  */
-export function toBigIntLE(buf: Buffer): BigInt {
+export function toBigIntLE(buf: Buffer): bigint {
   if (process.browser || converter === undefined) {
     const reversed = Buffer.from(buf);
     reversed.reverse();
@@ -40,7 +40,7 @@ export function toBigIntLE(buf: Buffer): BigInt {
  * @param buf The big-endian buffer to convert.
  * @returns A BigInt with the big-endian representation of buf.
  */
-export function toBigIntBE(buf: Buffer): BigInt {
+export function toBigIntBE(buf: Buffer): bigint {
   if (process.browser || converter === undefined) {
     const hex = buf.toString('hex');
     if (hex.length === 0) {
@@ -57,7 +57,7 @@ export function toBigIntBE(buf: Buffer): BigInt {
  * @param width The number of bytes that the resulting buffer should be.
  * @returns A little-endian buffer representation of num.
  */
-export function toBufferLE(num: BigInt, width: number): Buffer {
+export function toBufferLE(num: bigint, width: number): Buffer {
   if (process.browser || converter === undefined) {
     const hex = num.toString(16);
     const buffer =
@@ -75,7 +75,7 @@ export function toBufferLE(num: BigInt, width: number): Buffer {
  * @param width The number of bytes that the resulting buffer should be.
  * @returns A big-endian buffer representation of num.
  */
-export function toBufferBE(num: BigInt, width: number): Buffer {
+export function toBufferBE(num: bigint, width: number): Buffer {
   if (process.browser || converter === undefined) {
     const hex = num.toString(16);
     return Buffer.from(hex.padStart(width * 2, '0').slice(0, width * 2), 'hex');
